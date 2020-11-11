46ebe030b742a44ed15255d55760093e2fd43c93
Edgar López <edgar.lr.18@gmail.com>
2020-11-10 20:01:06 -0600

    Add tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,13 @@
-const index = () => <div>Hola</div>
+const Home = () => {
+  return (
+    <div>
+      <div className="py-20">
+        <h1 className="text-5xl text-center text-accent-1">
+          Next.js + Tailwind CSS
+        </h1>
+      </div>
+    </div>
+  )
+}
 
-export default index
+export default Home
