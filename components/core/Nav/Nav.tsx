b59b115b300e3dcf69068ebdd0cbde4d82a29d45
Edgar López <edgar.lr.18@gmail.com>
2020-11-11 19:55:24 -0600

    Add styles to Nav

--- components/core/Nav/Nav.tsx
+++ components/core/Nav/Nav.tsx
@@ -1,13 +1,19 @@
+import { IconList } from '@components/icons'
 import Link from 'next/link'
 
 const Nav = ({ categories }: { categories: TCategory[] }) => {
   return (
-    <nav className="overflow-x-scroll">
+    <nav className="overflow-x-scroll whitespace-no-wrap relative flex h-12 items-center">
       {categories.map((category) => (
         <Link href={`/${category.slug}`} key={category.slug}>
-          <a className="uppercase py-2 px-4">{category.title}</a>
+          <a className="uppercase py-2 px-4 text-sm font-bold">
+            {category.title}
+          </a>
         </Link>
       ))}
+      <button className="sticky py-2 pl-2 right-0  bg-primary">
+        <IconList />
+      </button>
     </nav>
   )
 }
