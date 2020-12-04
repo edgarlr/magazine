405f8b79dcfe7271d0240c33715a135a7507d012
Edgar López <edgar.lr.18@gmail.com>
2020-12-03 18:29:18 -0600

    Add active tab to nav

--- components/core/Nav/Nav.tsx
+++ components/core/Nav/Nav.tsx
@@ -1,11 +1,29 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import cn from 'classnames'
 
 const Nav = ({ categories }: { categories: TCategory[] }) => {
+  const router = useRouter()
   return (
     <nav className="overflow-x-scroll sticky flex h-14 items-center whitespace-nowrap top-14 px-4 z-10 bg-primary scrollbar-none">
+      <Link href={`/`}>
+        <a
+          className={cn(
+            'uppercase py-2 px-4 text-sm font-bold',
+            router.pathname === '/' && 'underline'
+          )}
+        >
+          HOME
+        </a>
+      </Link>
       {categories.map((category) => (
         <Link href={`/${category.slug}`} key={category.slug}>
-          <a className="uppercase py-2 px-4 text-sm font-bold">
+          <a
+            className={cn(
+              'uppercase py-2 px-4 text-sm font-bold',
+              router.query.slug === category.slug && 'underline'
+            )}
+          >
             {category.title}
           </a>
         </Link>
