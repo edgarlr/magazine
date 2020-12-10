0af1e98086172af5ee6ca1cbbd993696d5dd99a8
Edgar López <edgar.lr.18@gmail.com>
2020-12-10 13:59:09 -0600

    Add expire time to preview routes

--- pages/api/page-preview.ts
+++ pages/api/page-preview.ts
@@ -22,7 +22,9 @@ export default async function pagePreview(
   }
 
   // Enable Preview Mode by setting the cookies
-  res.setPreviewData({})
+  res.setPreviewData({
+    maxAge: 60 * 60, // The preview mode cookies expire in 1 hour
+  })
 
   // Redirect to the path from the fetched post
   // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
