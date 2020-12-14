536ae48dc4adffabd6c169694e9342910c700c2e
Edgar López <edgar.lr.18@gmail.com>
2020-12-14 14:57:32 -0600

    Add prev/next buttons to articles carousel

--- components/article/ArticlesCarousel/ArticlesCarousel.tsx
+++ components/article/ArticlesCarousel/ArticlesCarousel.tsx
@@ -1,4 +1,6 @@
+import { IconArrowLeft, IconArrowRight } from '@components/icons'
 import { ArticleCard } from '..'
+import { useRef, MouseEvent } from 'react'
 
 type Props = {
   articles: TArticle[]
@@ -6,12 +8,37 @@ type Props = {
 }
 
 const ArticlesCarousel = ({ title, articles }: Props) => {
+  const carouselRef = useRef<HTMLDivElement>(null)
+
+  const scrollToLeft = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    carouselRef.current?.scrollBy({ left: -1, top: 0, behavior: 'smooth' })
+  }
+  const scrollToRight = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    carouselRef.current?.scrollBy({ left: 1, top: 0, behavior: 'smooth' })
+  }
   return (
     <section className="mb-12">
       <div className="flex items-center justify-between h-auto py-4">
         <p className="uppercase">{title}</p>
+        <ul className="flex">
+          <li>
+            <button onClick={scrollToLeft} className="disabled:opacity-50">
+              <IconArrowLeft />
+            </button>
+          </li>
+          <li>
+            <button onClick={scrollToRight} className="disabled:opacity-50">
+              <IconArrowRight />
+            </button>
+          </li>
+        </ul>
       </div>
-      <div className="flex overflow-hidden overflow-x-scroll scroll-snap-x-mandatory scrollbar-none">
+      <div
+        ref={carouselRef}
+        className="flex overflow-hidden overflow-x-scroll scroll-snap-x-mandatory scrollbar-none"
+      >
         {articles.map((article) => (
           <ArticleCard
             article={article}
