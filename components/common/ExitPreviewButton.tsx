0fc849a952e6d68c5f2c1bde394f0d61f395eaac
Edgar López <edgar.lr.18@gmail.com>
2021-01-06 00:52:55 -0600

    Change pink accent color for a11y

--- components/common/ExitPreviewButton.tsx
+++ components/common/ExitPreviewButton.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link'
 const ExitPreviewButton = () => {
   return (
     <Link href="/api/exit-preview">
-      <h6 className="fixed bottom-0 p-3 left-0 right-0 bg-pink text-primary z-20 text-center uppercase">
+      <p className="fixed bottom-0 p-3 left-0 right-0 bg-pink text-primary z-20 text-center uppercase">
         Exit preview mode
-      </h6>
+      </p>
     </Link>
   )
 }
