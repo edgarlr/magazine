b67263217b8b0cde27d78740cf24158221981fd6
Edgar López <edgar.lr.18@gmail.com>
2021-01-07 16:38:23 -0600

    Rework search using local filters instead

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import { useSearch } from '@lib/hooks/use-search'
+// import { useSearch } from '@lib/hooks/use-search'
 import { ArticlesList } from '@components/article'
 import { fetchAPI } from '@lib/api'
 import { InferGetStaticPropsType } from 'next'
@@ -9,21 +9,52 @@ import { Layout } from '@components/common/Layout'
 
 export async function getStaticProps() {
   const categories: TCategory[] = await fetchAPI('/categories')
-  return { props: { categories } }
+  const articles: TArticle[] = await fetchAPI('/articles')
+  return { props: { categories, articles } }
 }
 
 function SearchPage({
   categories,
+  articles,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   const { query } = useRouter()
   const { q, category, sort } = query
-  const { isLoading, data } = useSearch<TArticle>('/articles', query)
+
+  // This hook should work with the API
+  // const { isLoading, data } = useSearch<TArticle>('/articles', query)
+
+  // I'm using a filter implementation due heroku sleep problem
+  const filteredArticles = articles.filter((a: TArticle) => {
+    const stringQuery = decodeURIComponent(q as string).toLowerCase()
+    if (category) {
+      return (
+        a.title.toLowerCase().includes(stringQuery) &&
+        a.category.slug === decodeURIComponent(category as string)
+      )
+    }
+    return a.title.toLowerCase().includes(stringQuery)
+  })
+
+  const sortedArticles = filteredArticles.sort((a: TArticle, b: TArticle) => {
+    if (sort === 'desc')
+      return a.published_at < b.published_at
+        ? 1
+        : a.published_at > b.published_at
+        ? -1
+        : 0
+    return a.published_at > b.published_at
+      ? 1
+      : a.published_at < b.published_at
+      ? -1
+      : 0
+  })
+
   return (
     <Layout>
       <h4>&quot;{query.q}&quot; in search</h4>
-      {isLoading && <p>Loading...</p>}
-      {data && data.length !== 0 ? (
-        <ArticlesList articles={data} title="Recent Articles" />
+      {/* {isLoading && <p>Loading...</p>} */}
+      {sortedArticles && sortedArticles.length !== 0 ? (
+        <ArticlesList articles={sortedArticles} title="Recent Articles" />
       ) : (
         <p>we couldn&apos;t find anything</p>
       )}
