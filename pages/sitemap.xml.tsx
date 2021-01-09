a5829d28d1da570f26ba989a34603536d4e1b9dd
Edgar López <edgar.lr.18@gmail.com>
2021-01-09 13:55:08 -0600

    Add robots.txt and page/sitemap

---
+++ pages/sitemap.xml.tsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import { SITE_URL } from '@lib/constants'
+import { fetchAPI } from '@lib/api'
+import { NextPageContext } from 'next'
+
+type Props = {
+  categories: TCategory[]
+  articles: TArticle[]
+  pages: TPage[]
+  contributors: TContributor[]
+}
+
+const createSitemap = ({
+  articles,
+  categories,
+  contributors,
+  pages,
+}: Props) => `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+    <url>
+      <loc>${SITE_URL}</loc>
+    </url>
+
+    ${categories
+      .map(({ slug }) => {
+        return `
+                <url>
+                    <loc>${`${SITE_URL}/${slug}`}</loc>
+                </url>
+            `
+      })
+      .join('')}
+    ${articles
+      .map(({ slug }) => {
+        return `
+                <url>
+                    <loc>${`${SITE_URL}/articles/${slug}`}</loc>
+                </url>
+            `
+      })
+      .join('')}
+    ${pages
+      .map(({ slug }) => {
+        return `
+                <url>
+                    <loc>${`${SITE_URL}/pages/${slug}`}</loc>
+                </url>
+            `
+      })
+      .join('')}
+
+    <url>
+      <loc>${`${SITE_URL}/contributors`}</loc>
+    </url>
+
+    ${contributors
+      .map(({ slug }) => {
+        return `
+                <url>
+                    <loc>${`${SITE_URL}/contributors/${slug}`}</loc>
+                </url>
+            `
+      })
+      .join('')}
+    </urlset>
+    `
+
+class Sitemap extends React.Component {
+  static async getInitialProps({ res }: NextPageContext) {
+    const categories: TCategory[] = await fetchAPI('/categories')
+    const articles: TArticle[] = await fetchAPI('/articles')
+    const pages: TPage[] = await fetchAPI('/pages')
+    const contributors: TContributor[] = await fetchAPI('/contributors')
+
+    res?.setHeader('Content-Type', 'text/xml')
+    res?.write(createSitemap({ categories, articles, pages, contributors }))
+    res?.end()
+  }
+}
+
+export default Sitemap
