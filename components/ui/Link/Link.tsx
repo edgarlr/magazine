01af2690aebb22a4b908f755ea10d92379438f22
Edgar López <edgar.lr.18@gmail.com>
2021-01-09 22:21:54 -0600

    Remove unused files

--- components/ui/Link/Link.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Link = () => {
-  return <div></div>
-}
-
-export default Link
--- components/ui/Text/Text.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Text = () => {
-  return <div></div>
-}
-
-export default Text
