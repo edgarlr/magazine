62bef3901b562756eefd10d2973e9215178aafad
Edgar López <edgar.lr.18@gmail.com>
2021-02-12 17:09:20 -0600

    Add articles carousel scroll indicator

--- components/article/ArticlesCarousel/ArticlesCarousel.tsx
+++ components/article/ArticlesCarousel/ArticlesCarousel.tsx
@@ -1,8 +1,10 @@
 import { ArticleCard } from '..'
-import { useRef, MouseEvent } from 'react'
+import { useRef, MouseEvent, useState, useEffect } from 'react'
 import ArrowLeft from '@components/icons/ArrowLeft'
 import ArrowRight from '@components/icons/ArrowRight'
 import { Button } from '@components/ui/Button'
+import cn from 'classnames'
+import { useIsMobile } from '@lib/hooks/use-media-queries'
 
 type Props = {
   articles: TArticle[]
@@ -10,7 +12,10 @@ type Props = {
 }
 
 const ArticlesCarousel = ({ title, articles }: Props) => {
+  const [scrollProgress, setScrollProgress] = useState(0)
   const carouselRef = useRef<HTMLDivElement>(null)
+
+  const isMobile = useIsMobile()
 
   const scrollToLeft = (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
@@ -22,22 +27,59 @@ const ArticlesCarousel = ({ title, articles }: Props) => {
     carouselRef.current?.scrollBy({ left: 1, top: 0, behavior: 'smooth' })
   }
 
+  const renderScrollIndicator = () => {
+    const selectedRange = Math.floor((scrollProgress * articles.length) / 110)
+
+    return articles.map((article, index) => (
+      <div
+        className={cn(
+          'w-2 h-2 mx-1 bg-primary-20 rounded-lg',
+          selectedRange === index ? 'bg-primary' : ''
+        )}
+        key={article.slug}
+      ></div>
+    ))
+  }
+
+  useEffect(() => {
+    if (!carouselRef.current) return
+    const element = carouselRef.current
+
+    const scrollListener = () => {
+      const windowScroll = element.scrollLeft
+      const totalWidth = element.scrollWidth - element.clientWidth
+
+      if (windowScroll === 0) {
+        return setScrollProgress(0)
+      }
+      if (windowScroll > totalWidth) {
+        return setScrollProgress(100)
+      }
+      return setScrollProgress((windowScroll / totalWidth) * 100)
+    }
+
+    element.addEventListener('scroll', scrollListener)
+    return () => element.removeEventListener('scroll', scrollListener)
+  }, [])
+
   return (
     <section className="mb-12">
       <div className="flex items-center justify-between h-auto py-4">
         <p className="uppercase">{title}</p>
-        <ul className="flex">
-          <li>
-            <Button onClick={scrollToLeft} ariaLabel="Previus article">
-              <ArrowLeft />
-            </Button>
-          </li>
-          <li>
-            <Button onClick={scrollToRight} ariaLabel="Next article">
-              <ArrowRight />
-            </Button>
-          </li>
-        </ul>
+        {!isMobile && (
+          <ul className="flex">
+            <li>
+              <Button onClick={scrollToLeft} ariaLabel="Previus article">
+                <ArrowLeft />
+              </Button>
+            </li>
+            <li>
+              <Button onClick={scrollToRight} ariaLabel="Next article">
+                <ArrowRight />
+              </Button>
+            </li>
+          </ul>
+        )}
       </div>
 
       <div
@@ -51,6 +93,10 @@ const ArticlesCarousel = ({ title, articles }: Props) => {
             variant="carousel"
           />
         ))}
+      </div>
+
+      <div className="flex justify-center h-auto mt-6 ">
+        {renderScrollIndicator()}
       </div>
     </section>
   )
