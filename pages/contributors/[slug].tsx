ac7077da1960fddaa2086077fcea0e4992f428db
Edgar López <edgar.lr.18@gmail.com>
2021-03-09 14:10:39 -0600

    Update styles at contributor page

--- pages/contributors/[slug].tsx
+++ pages/contributors/[slug].tsx
@@ -6,6 +6,7 @@ import ExternalLink from '@components/ui/Link/ExternalLink'
 import Image from 'next/image'
 import { Layout } from '@components/common/Layout'
 import Custom404 from 'pages/404'
+import Twitter from '@components/icons/Twitter'
 
 export async function getStaticPaths() {
   // If you don't have too many contributors you can uncomment
@@ -60,7 +61,7 @@ function ContributorPage({
     <Layout>
       <section className="text-center py-4">
         {isFeatured && (
-          <figure className="relative w-24 h-24 mx-auto my-2">
+          <figure className="relative w-24 h-24 mx-auto my-6">
             <Image
               src={thumbnailUrl}
               className="rounded-full"
@@ -69,18 +70,24 @@ function ContributorPage({
             />
           </figure>
         )}
-        <h3 className="serif">{contributor?.name}</h3>
-        <p className="text-xs uppercase text-secondary">{contributor?.role}</p>
+        <h3 className="serif mt-0">{contributor?.name}</h3>
+        <p className="text-sm font-serif uppercase  mb-2">
+          {contributor?.role}
+        </p>
         {contributor?.urls?.twitter && (
           <ExternalLink
             to={`https://twitter.com/${contributor?.urls.twitter}`}
             ariaLabel="Contributor's twitter"
+            className="flex w-max mx-auto items-center opacity-60 hover:opacity-100"
           >
+            <span className="mr-2">
+              <Twitter width="18" height="18" />
+            </span>
             {contributor?.urls.twitter}
           </ExternalLink>
         )}
         {isFeatured && (
-          <p className="text-center py-2 leading-tight">
+          <p className="text-center py-2 leading-tight mt-8">
             {contributor?.featured?.description}
           </p>
         )}
