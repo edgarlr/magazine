65d61fff095fd05ff75c7189a7ee0b93b581c51d
Edgar López <edgar.lr.18@gmail.com>
2021-03-09 14:02:59 -0600

    Add Articles hero

---
+++ components/article/ArticlesHero/ArticlesHero.tsx
@@ -0,0 +1,73 @@
+import { Date } from '@components/ui/Date'
+import { getMediaURL } from '@lib/api'
+import Link from 'next/link'
+import s from '../ArticleCard/ArticleCard.module.css'
+import cn from 'classnames'
+import Image from 'next/image'
+import ArticleCardTop from '../ArticleCard/ArticleCardTop'
+import ActionButtons from '../Article/ActionButtons'
+
+const ArticlesHero = ({ articles }: { articles: TArticle[] }) => {
+  return (
+    <section className="mb-4 flex justify-between items-center py-16 border-b">
+      <div style={{ width: '45%' }}>
+        <article className={s.hero}>
+          <Link href={`/articles/${articles[0].slug}`}>
+            <figure>
+              <Image
+                src={getMediaURL(articles[0].cover.url)}
+                alt={articles[0].cover.alternativeText || ''}
+                layout="fill"
+                className="object-cover"
+              />
+            </figure>
+          </Link>
+
+          <section className="pt-8">
+            <Link href={`/${articles[0].category.slug}`}>
+              <a className="uppercase text-sm font-bold px-2 py-1 text-accent border border-accent rounded-sm hover:underline">
+                {articles[0].category.title}
+              </a>
+            </Link>
+            <Link href={`/articles/${articles[0].slug}`}>
+              <h3
+                className={cn(
+                  s.title,
+                  'serif leading-tight overflow-hidden max-h-28 mt-4 mb-2 hover:underline'
+                )}
+              >
+                {articles[0].title}
+              </h3>
+            </Link>
+            <div className="flex text-sm">
+              By
+              <Link href={`/contributors/${articles[0].author.slug}`}>
+                <p className="pl-1 pr-2 font-bold hover:underline">
+                  {articles[0].author.name}
+                </p>
+              </Link>
+              {' | '}
+              <Date
+                className="px-2"
+                date={articles[0].published_at as string}
+              />
+            </div>
+          </section>
+          <ActionButtons article={articles[0]} />
+        </article>
+      </div>
+
+      <div style={{ width: '45%' }}>
+        {articles.slice(0, 4).map((article, index) => (
+          <ArticleCardTop
+            article={article}
+            index={index + 1}
+            key={article.slug}
+          />
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default ArticlesHero
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import { InferGetStaticPropsType } from 'next'
 import { ArticlesCarousel, ArticlesList } from '@components/article'
 import { fetchAPI, getNavigation } from '@lib/api'
 import { Layout } from '@components/common/Layout'
+import { useMediaQuery } from '@lib/hooks/use-media-queries'
+import ArticlesHero from '@components/article/ArticlesHero/ArticlesHero'
 
 export async function getStaticProps() {
   const articles: TArticle[] = await fetchAPI('/articles')
@@ -14,9 +16,16 @@ function Home({
   articles,
   navigation,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const isTablet = useMediaQuery(1023)
+
   return (
     <Layout navigation={navigation}>
-      <ArticlesCarousel title="Top stories" articles={articles} />
+      {isTablet ? (
+        //Tablet and smaller devices
+        <ArticlesCarousel title="Top stories" articles={articles} />
+      ) : (
+        <ArticlesHero articles={articles} />
+      )}
       <ArticlesList articles={articles} title="Recent" />
 
       <div className="lg:py-24 lg:flex lg:gap-28 lg:mx-auto">
