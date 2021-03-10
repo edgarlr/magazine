2ed4c9942f7f4578de56d053dfa515837a7225a9
Edgar López <edgar.lr.18@gmail.com>
2021-03-10 17:14:12 -0600

    Update ExitPreviewButton styles

--- components/common/ExitPreviewButton.tsx
+++ components/common/ExitPreviewButton.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const ExitPreviewButton = () => {
   return (
     <Link href="/api/exit-preview">
-      <p className="fixed bottom-0 p-3 left-0 right-0 bg-pink text-primary z-20 text-center uppercase">
+      <p className="fixed bottom-4 text-sm rounded-lg p-3 left-6 right-6 bg-accent text-secondary font-bold z-20 text-center uppercase cursor-pointer transform scale-100 transition-transform hover:scale-105 lg:w-1/2 lg:mx-auto">
         Exit preview mode
       </p>
     </Link>
