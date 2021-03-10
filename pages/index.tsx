87f4cb417b1b90c6a18060469fa53ed63a8c99fe
Edgar López <edgar.lr.18@gmail.com>
2021-03-10 15:34:46 -0600

    Add limits to index articles list

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,27 +22,27 @@ function Home({
     <Layout navigation={navigation}>
       {isTablet ? (
         //Tablet and smaller devices
-        <ArticlesCarousel title="Top stories" articles={articles} />
+        <ArticlesCarousel title="Top stories" articles={articles.slice(0, 4)} />
       ) : (
-        <ArticlesHero articles={articles} />
+        <ArticlesHero articles={articles.slice(0, 4)} />
       )}
 
-      <ArticlesList articles={articles} title="Recent" />
+      <ArticlesList articles={articles.slice(5, 10)} title="Recent" />
 
       <div className="lg:py-24 lg:flex lg:gap-28 lg:mx-auto">
         <ArticlesList
-          articles={articles}
+          articles={articles.slice(0, 4)}
           title="Articulos Principales"
           variant="top"
         />
         <ArticlesList
-          articles={articles}
+          articles={articles.slice(5, 10)}
           title="Artículos Más leidos"
           variant="top"
         />
       </div>
 
-      <ArticlesList articles={articles} title="More articles" />
+      <ArticlesList articles={articles.slice(10, 15)} title="More articles" />
     </Layout>
   )
 }
